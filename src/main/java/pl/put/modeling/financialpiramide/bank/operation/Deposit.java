ee4f9275e65e1957f80c8145010f4b5182d6a614
MateuszPaterak <mzt@onet.eu>
2017-11-05 09:18:02 +0100

    add empty operations

---
+++ src/main/java/pl/put/modeling/financialpiramide/bank/operation/Deposit.java
@@ -0,0 +1,8 @@
+package pl.put.modeling.financialpiramide.bank.operation;
+
+public class Deposit implements  Operation{
+    @Override
+    public boolean operate() {
+        return false;
+    }
+}
---
+++ src/main/java/pl/put/modeling/financialpiramide/bank/operation/Transfer.java
@@ -0,0 +1,8 @@
+package pl.put.modeling.financialpiramide.bank.operation;
+
+public class Transfer implements Operation {
+    @Override
+    public boolean operate() {
+        return false;
+    }
+}

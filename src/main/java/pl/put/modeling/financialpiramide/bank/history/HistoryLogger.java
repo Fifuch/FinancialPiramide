8d13fae7c85f36cabf029caca1dc1babbc04effe
Filip Sochal <fsochal@gmail.com>
2017-11-05 10:01:01 +0100

    Configured AspectJ and worked further on HistoryLogger.

--- src/main/java/pl/put/modeling/financialpiramide/bank/history/HistoryLogger.java
+++ src/main/java/pl/put/modeling/financialpiramide/bank/history/HistoryLogger.java
@@ -1,7 +1,24 @@
 package pl.put.modeling.financialpiramide.bank.history;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 import pl.put.modeling.financialpiramide.bank.Bank;
 
+import javax.inject.Inject;
+
+@Aspect
 public class HistoryLogger {
     private Bank bank;
+
+    @Inject
+    public HistoryLogger(Bank bank) {
+        this.bank = bank;
+    }
+
+    @Around("execution(* pl.put.modeling.financialpiramide.bank.operation.Operation.operate(..))")
+    public void addHistoryEntry(JoinPoint operation) {
+
+    }
+
 }

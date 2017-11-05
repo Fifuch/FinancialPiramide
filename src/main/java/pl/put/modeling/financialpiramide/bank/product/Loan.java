7f24d025490fc913c9f786cc3c8303ddf871224f
Kamil Kołodziej <kamil.kolodziej@student.put.poznan.pl>
2017-11-05 08:13:53 +0100

    + added account model

---
+++ src/main/java/pl/put/modeling/financialpiramide/bank/product/Loan.java
@@ -0,0 +1,4 @@
+package pl.put.modeling.financialpiramide.bank.product;
+
+public class Loan implements Product {
+}
---
+++ src/main/java/pl/put/modeling/financialpiramide/bank/product/Placement.java
@@ -0,0 +1,4 @@
+package pl.put.modeling.financialpiramide.bank.product;
+
+public class Placement implements Product {
+}

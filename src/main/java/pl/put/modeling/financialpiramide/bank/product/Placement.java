93f8ad6f3099bcbf658ef6fc9e89654e299e348c
Kamil Kołodziej <kamil.kolodziej@student.put.poznan.pl>
2017-11-05 08:23:48 +0100

    + added account model

--- src/main/java/pl/put/modeling/financialpiramide/bank/product/Placement.java
+++ src/main/java/pl/put/modeling/financialpiramide/bank/product/Placement.java
@@ -1,4 +1,60 @@
 package pl.put.modeling.financialpiramide.bank.product;
 
+import pl.put.modeling.financialpiramide.bank.interest.InterestSystem;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Placement implements Product {
+  
+    @Override
+    public List<Product> getProducts() {
+        return null;
+    }
+
+    @Override
+    public void setProducts(List<Product> products) {
+
+    }
+
+    @Override
+    public InterestSystem getInterests() {
+        return null;
+    }
+
+    @Override
+    public void setInterests(InterestSystem interests) {
+
+    }
+
+    @Override
+    public LocalDateTime getCreationDate() {
+        return null;
+    }
+
+    @Override
+    public void setCreationDate(LocalDateTime creationDate) {
+
+    }
+
+    @Override
+    public Long getAccountNumber() {
+        return null;
+    }
+
+    @Override
+    public void setAccountNumber(Long accountNumber) {
+
+    }
+
+    @Override
+    public BigDecimal getBalance() {
+        return null;
+    }
+
+    @Override
+    public void setBalance(BigDecimal balance) {
+
+    }
 }
--- src/main/java/pl/put/modeling/financialpiramide/bank/product/Product.java
+++ src/main/java/pl/put/modeling/financialpiramide/bank/product/Product.java
@@ -1,4 +1,31 @@
 package pl.put.modeling.financialpiramide.bank.product;
 
+import pl.put.modeling.financialpiramide.bank.interest.InterestSystem;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface Product {
+
+    List<Product> getProducts();
+
+    void setProducts(List<Product> products);
+
+    InterestSystem getInterests();
+
+    void setInterests(InterestSystem interests);
+
+    LocalDateTime getCreationDate();
+
+    void setCreationDate(LocalDateTime creationDate);
+
+    Long getAccountNumber();
+
+    void setAccountNumber(Long accountNumber);
+
+    BigDecimal getBalance();
+
+    void setBalance(BigDecimal balance);
+
 }
